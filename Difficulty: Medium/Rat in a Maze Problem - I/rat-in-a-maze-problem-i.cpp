8c48b5a78ca30afbd0515c48bb4d1587842a5315
Sharad Chandra Reddy <97039791+SCR01@users.noreply.github.com>
2024-08-13 22:26:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -9,75 +9,64 @@ using namespace std;
 // User function template for C++
 
 class Solution {
-    
-      public:
-    vector<string>result;
-    
-    bool safe(int i,int j,int n){
-        
-        return i>=0 && j>=0 && i<n && j<n;
-    }
-    
-    void solve(int i,int j,vector<vector<int>> &mat,int n,string &path){
-        
-       
-       if(!safe(i,j,n)|| mat[i][j]==0){
-           return;
-       } 
-       
-       if(i==n-1 && j==n-1){
-           
-           
-           
-           result.push_back(path);
-           
-           return;
-       }
-       
-       
-       mat[i][j]=0;
-       
-      path.push_back('L');
-      
-      solve(i,j-1,mat,n,path);
-      
-      path.pop_back();
-      
-        path.push_back('R');
-      
-      solve(i,j+1,mat,n,path);
-      
-      path.pop_back();
-      
-        path.push_back('U');
-      
-      solve(i-1,j,mat,n,path);
-      
-      path.pop_back();
-      
-        path.push_back('D');
-      
-      solve(i+1,j,mat,n,path);
-      
-      path.pop_back();
-      
-      mat[i][j]=1;
-      
-        
-        
-    }
+  public:
+  
+  void maze( vector <vector<int> > &arr,int r,int c,int row,int col,string &temp,vector <string> &ans,vector <vector <int> > &visit){
+     if(r==row-1 && c==col-1){
+        ans.push_back(temp);
+        return;
+     }
 
-    vector<string> findPath(vector<vector<int>> &mat) {
-        
-        
-        int n=mat.size();
+
+    //up direction
+    if(r-1 >=0 && visit[r-1][c] ==0 && arr[r-1][c] == 1){
+        visit[r-1][c] = 1;
+        temp.push_back('U');
+        maze(arr,r-1,c,row,col,temp,ans,visit);
+        temp.pop_back();
+        visit[r-1][c] = 0;
+    }
+    //down direction
+
+    if(r+1 <row && visit[r+1][c] ==0 && arr[r+1][c] == 1){
+        visit[r+1][c] = 1;
+        temp.push_back('D');
+        maze(arr,r+1,c,row,col,temp,ans,visit);
+        temp.pop_back();
+        visit[r+1][c] = 0;
+    }
+    //left
+    if(c-1 >=0 && visit[r][c-1] ==0 && arr[r][c-1] == 1){
+        visit[r][c-1] = 1;
+        temp.push_back('L');
+        maze(arr,r,c-1,row,col,temp,ans,visit);
+        temp.pop_back();
+        visit[r][c-1] = 0;
+    }
+    //right
+    if(c+1 <col && visit[r][c+1] ==0 && arr[r][c+1] == 1){
+        visit[r][c+1] = 1;
+        temp.push_back('R');
+        maze(arr,r,c+1,row,col,temp,ans,visit);
+        temp.pop_back();
+        visit[r][c+1] = 0;
+    }
+}
+
+    vector<string> findPath(vector<vector<int>> &arr) {
+        // Your code goes here
+     int row = arr.size();
+    int col = arr[0].size();
+    string temp;
+    vector <string> ans;
+    vector <vector <int> > visit(row,(vector <int> (col,0)));
+
+    if (arr[0][0] == 1 && arr[row - 1][col - 1] == 1) {
+        visit[0][0] = 1;
+        maze(arr, 0, 0, row, col, temp, ans, visit);
+    }
     
-    
-    string path;
-      
-     
-      solve(0,0,mat,n,path);
-      return result;
+    return ans;
     }
 };
 
